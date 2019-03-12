0d3f6f905dfdb55fbe1d00e0ef4f5d49f2d1b2b7
Shadowgasher1 <aashwood@aggies.ncat.edu>
2019-03-12 14:52:22 -0400

    save changes sprint_1

--- group_8.py
+++ group_8.py
@@ -73,7 +73,7 @@ def tc_dna_intent_api_v1_network_device_config():
     if response.status_code == 200:
         print("Correct status code")
         print("Status code =",response.status_code)
-    #pp.pprint(response.json())
+        #pp.pprint(response.json())
     else:
         print("Incorrect status code")
         print("Status code =", response.status_code)
@@ -127,8 +127,8 @@ def run_all_tests():
 
     # add test cases to these methods
     tc_dna_intent_api_v1_network_device_config()
-    tc_dna_intent_api_v1_network_device_config_count()
-    tc_dna_intent_api_v1_network_device_config_device()
+    #tc_dna_intent_api_v1_network_device_config_count()
+    #tc_dna_intent_api_v1_network_device_config_device()
 
 
 
