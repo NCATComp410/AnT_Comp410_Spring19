2d07e9b277f8ba0b453a889df96c96ee6807fe6a
Alexis Cooper <adcoope2@aggies.ncat.edu>
2019-03-15 17:10:12 -0400

    Mock template

--- group_2.py
+++ group_2.py
@@ -1,6 +1,8 @@
 from utils import DnaCenter
 from utils import TestCase
 import pprint
+import responses
+import requests
 
 # define a pretty-printer for diagnostics
 pp = pprint.PrettyPrinter(indent=4)
@@ -71,24 +73,53 @@ def tc_dna_intent_api_v1_topology_l3_ospf():
     # complete
     tc.okay('complete')
 
+use_intent = True
+
+if use_intent:
+    intent_api = 'dna/intent/'
+else:
+    intent_api = ''
+
+use_mock = True
+@responses.activate
+
+
 
 def tc_dna_intent_api_v1_topology_l3_isis():
     #Alexis Cooper
     # create this test case
+
+
     tc = TestCase(test_name='IntentApiV1TopologyL3Isis', yaml_file='params.yaml')
 
-    # create a session to the DNA-C
-    dnac = DnaCenter(hostname=tc.params['DnaCenter']['Hostname'],
-                     port=tc.params['DnaCenter']['Port'],
-                     username=tc.params['DnaCenter']['Username'],
-                     password=tc.params['DnaCenter']['Password'])
 
-    # execute the command and get response
-    response = dnac.get('dna/intent/api/v1/topology/l3/isis')
+   # execute the command and get response
+   # response = dnac.get('dna/intent/api/v1/topology/l3/isis')
     # pp.pprint(response.json())
     # ####################################################################
-    # ###EDITS####
-    # Check to see if a response other than 200-OK was received
+
+    rest_cmd = intent_api + 'api/v1/network-device'
+    if not use_mock:
+        # In this case we don't want to use a mock and will create a normal session to the dnac
+        # create a session to the DNA-C and get a response back
+        dnac = DnaCenter(hostname=tc.params['DnaCenter']['Hostname'],
+                         port=tc.params['DnaCenter']['Port'],
+                         username=tc.params['DnaCenter']['Username'],
+                         password=tc.params['DnaCenter']['Password'])
+
+        # get list of all network devices
+        response = dnac.get(rest_cmd)
+    else:
+        # In this case we're going to use a mock and not actually communicate with the DNA-C
+        # The json_mock is something which was saved in responses.log during a successful session
+        # It is much easier to build this way than to build it from scratch!
+        json_mock = {'response': 4, 'version': '1.0'}
+
+        responses.add(responses.GET, 'http://' + rest_cmd,
+                      json=json_mock,
+                      status=200)
+        response = requests.get('http://' + rest_cmd)
+
     if response.status_code != 200:
         # this test should fail if any other response code received
         tc.fail('expected 200-OK actual response was ' + str(response.status_code))
@@ -132,9 +163,9 @@ def run_all_tests():
     tc_dna_intent_api_v1_network_device_count()
 
     # add new test cases to be run here
-    tc_dna_intent_api_v1_topology_l3_ospf()
+   # tc_dna_intent_api_v1_topology_l3_ospf()
     tc_dna_intent_api_v1_topology_l3_isis()
-    tc_dna_intent_api_v1_topology_l3_static()
+   # tc_dna_intent_api_v1_topology_l3_static()
 
 
 run_all_tests()
