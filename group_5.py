2da6ee3a2da7116806a6112387003345c9497f02
bria0705 <bpmassey@aggies.ncat.edu>
2019-04-29 22:03:29 -0400

    Update group_5.py

--- group_5.py
+++ group_5.py
@@ -528,8 +528,8 @@ def tc_dna_intent_api_v1_interface_network_device_range():
             response = requests.get('http://' + rest_cmd)
             
         if response.status_code != 200:
-        # this test should fail if any other response code received
-        tc.fail('expected 200-OK actual response was ' + str(response.status_code))
+            # this test should fail if any other response code received
+            tc.fail('expected 200-OK actual response was ' + str(response.status_code))
         else:
             for field in response.json()['response']:
                 # show the fields in this response
